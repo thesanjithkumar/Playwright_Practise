86b58a55814aac6685c617e32c383ff347214876
Sanjith Kumar <sanjithkumar048@gmail.com>
2023-02-09 18:18:43 +0530

    commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,19 @@ test('get started link', async ({ page }) => {
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
 });
+
+test('test login', async ({ page }) => {
+  await page.goto('localhost:3000/login');
+
+  // Click the get started link.
+  // await page.getByRole('link', { name: 'Sign in' }).click();
+
+  // Expects the URL to contain intro.
+  // await expect(page).toHaveURL(/.*login*./);
+  await page.getByLabel("Username").fill("testUser")
+  await page.getByLabel("Password").fill("1234")
+  // await expect(page.getByLabel("Username")).toContainText('testUser')
+  // await expect(page.getByLabel("Password")).toContainText('1234')
+  await page.getByRole("button", { name: "login" }).click()
+  await expect(page.getByText('Instantly deploy your Next.js site to a shareable URL with Vercel.')).toBeVisible();
+});
